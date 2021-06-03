cc14ac6fdcdfbba03c364c4a45ec0b57afb5722b
= <pooyadav100691@gmail.com>
2021-06-03 11:16:35 +0700

    Update setup basic security

--- src/main/java/me/tedory/springsecuritydemo/NoneProtectedController.java
+++ src/main/java/me/tedory/springsecuritydemo/NoneProtectedController.java
@@ -6,11 +6,10 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("/api")
-public class AdminProtectedRestController {
-
-    @GetMapping("/hiddenmessage")
-    public ResponseEntity<?> getAdminProtectedGreeting() {
-        return ResponseEntity.ok("this is a hidden message!");
+@RequestMapping("/public")
+public class NoneProtectedController {
+    @GetMapping("/message")
+    public ResponseEntity<?> getGetMessage() {
+        return ResponseEntity.ok("this is a none protected message!");
     }
 }
